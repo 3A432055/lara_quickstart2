4c10cd9d7a6a9b76e22ba32d3bd3ab0e0454b2f8
3A432055 <david860120@gmail.com>
2019-12-11 10:31:18 +0800

    刪除任務

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Policies;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Task;
+class TaskPolicy
+{
+    use HandlesAuthorization;
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    /**
+     * 判斷當給定的使用者可以刪除給定的任務。
+     *
+     * @param  User  $user
+     * @param  Task  $task
+     * @return bool
+     */
+    public function destroy(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
+}
