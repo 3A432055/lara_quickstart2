a0079e8a327511bc3d0e28c8c69fcad928174828
3A432055 <david860120@gmail.com>
2019-12-11 09:49:13 +0800

    將回傳視圖的程式碼 view('tasks.index');

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
 }
+
+
