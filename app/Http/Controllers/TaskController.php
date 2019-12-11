3413db169f379b2e8e5fa50da790ca9e93805584
3A432055 <david860120@gmail.com>
2019-12-11 10:11:49 +0800

    顯示已有的任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,9 +6,14 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    //顯示已有的任務
     public function index(Request $request)
     {
-        return view('tasks.index');
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
     }
     public function store(Request $request)
     {
@@ -16,8 +21,17 @@ class TaskController extends Controller
             'name' => 'required|max:255',
         ]);
 
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+
         // Create The Task...
     }
+
+
+
 
 }
 
